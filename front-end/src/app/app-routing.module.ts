d9dd17d55cdd6e74f325d258985a1f428eb8d554
Filipp <pilip.pashkevich@gmail.com>
2020-11-12 18:57:08 +0300

    creating auth panel & register panel

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {RegComponent} from "./reg/reg.component";
+import {AuthComponent} from "./auth/auth.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {AuthGuard} from "./auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'reg', component: RegComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { RegComponent } from './reg/reg.component';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import {FormsModule} from "@angular/forms";
+import {FlashMessagesModule} from "angular2-flash-messages";
+import {AuthService} from "./auth.service";
+import {HttpModule} from "@angular/http";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    RegComponent,
+    AuthComponent,
+    DashboardComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        FlashMessagesModule,
+        FlashMessagesModule.forRoot(),
+        HttpModule
+    ],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
